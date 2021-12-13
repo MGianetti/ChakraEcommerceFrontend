20ea1587471047c3547c7f92c54fe2d16aee25f2
Mateus Jesus <mateus_jesus@apple.com>
2021-12-13 12:24:05 -0300

    Finished ProductCard

--- src/components/products-list-page/product-card/product-card.component.jsx
+++ src/components/products-list-page/product-card/product-card.component.jsx
@@ -3,15 +3,16 @@ import { FaCartPlus } from 'react-icons/fa';
 
 export default function ProductCard(props) {
   return (
-    <Box w="100%" h="200" borderWidth="1px" borderRadius="lg" boxShadow="md">
-      <Flex alignItems="center" margin="0px 30px" direction="column">
+    <Box w="100%" h="250" borderWidth="1px" borderRadius="lg" boxShadow="md">
+      <Flex alignItems="center" margin="30px" direction="column">
         <Flex alignItems="center" justify="space-between" w="100%">
-          <Heading>Tênis</Heading>
+          <Heading size="lg">Tênis</Heading>
           <Badge size="xs" colorScheme="green" height="max-content">
             $ 200
           </Badge>
         </Flex>
-        <Text margin="10px" maxHeight="90px" overflowY="scroll">
+        <Heading size="xs">Descrição</Heading>
+        <Text margin="10px" maxHeight="90px" overflowY="scroll" border="1px">
           Aenean non lorem nisl. Vivamus vel justo malesuada, egestas nisl vel,
           molestie leo. Aenean non lorem nisl. Vivamus vel justo malesuada,
           egestas nisl vel, molestie leo. Aenean non lorem nisl. Vivamus vel
@@ -20,7 +21,7 @@ export default function ProductCard(props) {
           Aenean non lorem nisl. Vivamus vel justo malesuada, egestas nisl vel,
           molestie leo.
         </Text>
-        <Flex w="100%" justifyContent="end" marginTop="5px">
+        <Flex w="100%" justifyContent="end" marginTop="15px">
           <FaCartPlus width="100%" />
         </Flex>
       </Flex>
