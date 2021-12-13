c47bd8050f9ff2000209fdb5b04a2ae65b9ed8b7
Mateus Jesus <mateus_jesus@apple.com>
2021-12-12 22:23:04 -0300

    Implemented Logged in layout

--- src/components/logged-layout/logged-layout.component.jsx
+++ src/components/logged-layout/logged-layout.component.jsx
@@ -1,5 +1,55 @@
+import {
+  Box,
+  Container,
+  Flex,
+  Link,
+  Menu,
+  Tag,
+  MenuButton,
+  MenuItem,
+  MenuList,
+  Spacer,
+} from '@chakra-ui/react';
+
+import { RiShoppingCartLine } from 'react-icons/ri';
+
 export default function LoggedLayout(props) {
   const { children } = props;
 
-  return <>{children}</>;
+  return (
+    <>
+      <Box as="nav" width="100%" height="100px" bg="green.200">
+        <Container>
+          <Flex
+            justify="space-around"
+            alignItems="center"
+            alignContent="center"
+            height="100px"
+          >
+            <Flex width="400px">
+              <Link>Produtos</Link>
+              <Spacer />
+              <Link>Pedidos</Link>
+              <Spacer />
+              <Link>Novo Produto</Link>
+            </Flex>
+            <Flex>
+              <Menu>
+                <MenuButton>
+                  <RiShoppingCartLine size={40} />
+                  <Tag borderRadius="full">10</Tag>
+                </MenuButton>
+                <MenuList>
+                  <MenuItem>Ver carrinho</MenuItem>
+                  <MenuItem>Logout</MenuItem>
+                </MenuList>
+              </Menu>
+            </Flex>
+          </Flex>
+        </Container>
+      </Box>
+      {children}
+      <Box mx="auto" as="footer" width="100%" height="80px" bg="green.200" />
+    </>
+  );
 }
