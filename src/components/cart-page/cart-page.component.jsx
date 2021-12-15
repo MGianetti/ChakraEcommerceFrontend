aacc95e4113e21857eadf42b94037afa461f8d31
Mateus Jesus <mateus_jesus@apple.com>
2021-12-15 17:41:55 -0300

    Implemented place orcer logic

--- src/components/cart-page/cart-page.component.jsx
+++ src/components/cart-page/cart-page.component.jsx
@@ -7,22 +7,40 @@ import {
 } from '@chakra-ui/react';
 import { useEffect, useState } from 'react';
 
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
 
 import ChartItem from './cart-item/cart-item.component';
+import { clearCart } from '../../store/cart/cartSlice';
+import { createNewOrder } from '../../services/orders-service/orders-service.service';
 
 export default function CartPage(props) {
   const cartItems = useSelector(state => state.cart.items);
   const [totalValue, setTotalValue] = useState(0);
+  const [isOrderFinishingLoading, setIsOrderFinishingLoading] = useState(false);
+
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
   useEffect(() => {
     setTotalValue(
       cartItems.reduce(
-        (accValue, currentItem) => accValue + currentItem.price,
+        (cartValue, currentItem) => cartValue + currentItem.price,
         0
       )
     );
   }, [cartItems]);
+
+  const handleFinishOrder = async () => {
+    setIsOrderFinishingLoading(true);
+    const productsIdList = cartItems.reduce((orderItems, cartItem) => {
+      return [...orderItems, cartItem.id];
+    }, []);
+    await createNewOrder({ productsIdList });
+    dispatch(clearCart());
+    setIsOrderFinishingLoading(false);
+    alert('Seu pedido foi realizado!');
+  };
 
   return (
     <>
@@ -30,20 +48,32 @@ export default function CartPage(props) {
         <Flex justify="center" padding="40px 0">
           <Heading>Meu carrinho</Heading>
         </Flex>
+        {cartItems.length === 0 && (
+          <Heading size="md">Seu carrinho está vazio</Heading>
+        )}
         <Flex w="100%" maxHeight="300px" direction="column" overflowY="scroll">
           {cartItems.map(cartItem => (
             <ChartItem cartItem={cartItem} />
           ))}
         </Flex>
         <Heading w="100%" textAlign="right" marginTop="25px">
-          Total: ${totalValue}
+          Total: ${totalValue.toFixed(2)}
         </Heading>
         <Flex justify="center" alignItems="center" height="200px">
           <ButtonGroup variant="outline" spacing="6">
-            <Button variant="ghost" colorScheme="gray">
+            <Button
+              variant="ghost"
+              colorScheme="gray"
+              onClick={() => navigate('/')}
+            >
               Voltar
             </Button>
-            <Button variant="solid" colorScheme="green">
+            <Button
+              variant="solid"
+              colorScheme="green"
+              onClick={handleFinishOrder}
+              isLoading={isOrderFinishingLoading}
+            >
               Finalizar pedido
             </Button>
           </ButtonGroup>
