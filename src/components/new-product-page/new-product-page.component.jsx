a6f9a7f4b0a40b5e2972f5f8511d68660ae0114e
Mateus Jesus <mateus_jesus@apple.com>
2021-12-15 13:25:39 -0300

    Implemented NewProductPage logic

--- src/components/new-product-page/new-product-page.component.jsx
+++ src/components/new-product-page/new-product-page.component.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import {
   FormControl,
   FormLabel,
@@ -8,9 +9,59 @@ import {
   ButtonGroup,
   Heading,
   Textarea,
+  FormErrorMessage,
 } from '@chakra-ui/react';
 
+import { createNewItem } from '../../services/items-service/items-service.service';
+import { addItem } from '../../store/items/itemsSlice';
+import { useDispatch } from 'react-redux';
+
 export default function NewProductPage(props) {
+  const [isLoginLoading, setIsLoginLoading] = useState(false);
+  const [hasInputErrors, setInputErrors] = useState(false);
+
+  const [newProductData, setNewProductData] = useState({
+    price: 0,
+    description: '',
+    productName: '',
+  });
+
+  const dispatch = useDispatch();
+
+  const handleFormFieldChange = (event, formKey) => {
+    setNewProductData({
+      ...newProductData,
+      [formKey]: event.currentTarget.value,
+    });
+  };
+
+  const handleRegisterClick = async e => {
+    if (isValidForm()) {
+      setIsLoginLoading(true);
+      await createNewItem(newProductData);
+      dispatch(addItem(newProductData));
+      setIsLoginLoading(false);
+      setNewProductData({
+        price: 0,
+        description: '',
+        productName: '',
+      });
+      alert('Produto criado com sucesso!');
+    }
+  };
+
+  const isValidForm = () => {
+    const { price, description, productName } = newProductData;
+    if (price === '' || description === '' || productName === '') {
+      setInputErrors(true);
+      return false;
+    }
+    setInputErrors(false);
+    return true;
+  };
+
+  const { productName, description, price } = newProductData;
+
   return (
     <>
       <Container maxWidth="1080px">
@@ -23,13 +74,29 @@ export default function NewProductPage(props) {
           direction="row"
           justifyContent="center"
         >
-          <FormControl id="email" width="400px">
+          <FormControl id="email" width="400px" isInvalid={hasInputErrors}>
             <FormLabel>Nome do produto</FormLabel>
-            <Input type="email" />
+            <Input
+              type="text"
+              value={productName}
+              onChange={event => handleFormFieldChange(event, 'productName')}
+            />
+            <FormErrorMessage>Digite um nome válido</FormErrorMessage>
             <FormLabel>Descrição</FormLabel>
-            <Textarea maxH="200px" type="password" />
+            <Textarea
+              maxH="200px"
+              type="text"
+              value={description}
+              onChange={event => handleFormFieldChange(event, 'description')}
+            />
+            <FormErrorMessage>Digite uma descrição válida</FormErrorMessage>
             <FormLabel>Preço</FormLabel>
-            <Input type="number" />
+            <Input
+              type="number"
+              value={price}
+              onChange={event => handleFormFieldChange(event, 'price')}
+            />
+            <FormErrorMessage>Digite um preço válido</FormErrorMessage>
           </FormControl>
         </Flex>
         <Flex justify="center" alignItems="center" height="200px">
@@ -37,7 +104,12 @@ export default function NewProductPage(props) {
             <Button variant="ghost" colorScheme="gray">
               Cancelar
             </Button>
-            <Button variant="solid" colorScheme="green">
+            <Button
+              variant="solid"
+              colorScheme="green"
+              isLoading={isLoginLoading}
+              onClick={handleRegisterClick}
+            >
               Cadastrar
             </Button>
           </ButtonGroup>
