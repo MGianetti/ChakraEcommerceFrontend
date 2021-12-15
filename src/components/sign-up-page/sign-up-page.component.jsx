d9604f565f8256d3d46c641e3d8bf75b883ddef0
Mateus Jesus <mateus_jesus@apple.com>
2021-12-14 22:28:01 -0300

    Implemented log in

--- src/components/sign-up-page/sign-up-page.component.jsx
+++ src/components/sign-up-page/sign-up-page.component.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import {
   FormControl,
   FormLabel,
@@ -8,11 +9,55 @@ import {
   ButtonGroup,
   Heading,
   Link,
+  FormErrorMessage,
 } from '@chakra-ui/react';
+
+import { signUpNewUser } from '../../services/auth-service/auth-service.service';
 
 import { Link as ReachLink } from 'react-router-dom';
 
 export default function SignUpPage(props) {
+  const [isSignUpLoading, setIsSignUpLoading] = useState(false);
+  const [hasInputErrors, setInputErrors] = useState(false);
+
+  const [userSignUp, setUserSignUp] = useState({
+    email: '',
+    password: '',
+    userName: '',
+    name: '',
+  });
+
+  const handleFormFieldChange = (event, formKey) => {
+    setUserSignUp({ ...userSignUp, [formKey]: event.currentTarget.value });
+  };
+
+  const isValidForm = () => {
+    const { email, password, userName, name } = userSignUp;
+
+    if (email === '' || password === '' || userName === '' || name === '') {
+      setInputErrors(true);
+      return false;
+    }
+    setInputErrors(false);
+    return true;
+  };
+
+  const handleSignUpClick = async e => {
+    if (isValidForm()) {
+      setIsSignUpLoading(true);
+      await signUpNewUser(userSignUp);
+      setIsSignUpLoading(false);
+      setUserSignUp({
+        email: '',
+        password: '',
+        userName: '',
+        name: '',
+      });
+    }
+  };
+
+  const { email, password, userName, name } = userSignUp;
+
   return (
     <>
       <Container justify="center">
@@ -20,16 +65,34 @@ export default function SignUpPage(props) {
           <Flex justify="center" height="150px">
             <Heading>Cadastre seu usuário</Heading>
           </Flex>
-          <Flex justify="center" alignItems="center" height="200px">
-            <FormControl id="email" width="400px">
+          <Flex justify="center" alignItems="center" height="300px">
+            <FormControl id="email" width="400px" isInvalid={hasInputErrors}>
               <FormLabel>Endereço de email</FormLabel>
-              <Input type="email" />
+              <Input
+                type="email"
+                onChange={event => handleFormFieldChange(event, 'email')}
+                value={email}
+              />
+              <FormErrorMessage>Digite um email válido</FormErrorMessage>
               <FormLabel>Senha</FormLabel>
-              <Input type="password" />
+              <Input
+                type="password"
+                onChange={event => handleFormFieldChange(event, 'password')}
+                value={password}
+              />
+              <FormErrorMessage>Digite uma senha válida</FormErrorMessage>
               <FormLabel>Nome de usuário</FormLabel>
-              <Input />
+              <Input
+                onChange={event => handleFormFieldChange(event, 'userName')}
+                value={userName}
+              />
+              <FormErrorMessage>Digite um usuário válido</FormErrorMessage>
               <FormLabel>Nome completo</FormLabel>
-              <Input />
+              <Input
+                onChange={event => handleFormFieldChange(event, 'name')}
+                value={name}
+              />
+              <FormErrorMessage>Digite um nome válido</FormErrorMessage>
             </FormControl>
           </Flex>
           <Flex justify="center" alignItems="center" height="200px">
@@ -39,7 +102,12 @@ export default function SignUpPage(props) {
                   Voltar
                 </Link>
               </Button>
-              <Button variant="solid" colorScheme="green">
+              <Button
+                variant="solid"
+                colorScheme="green"
+                isLoading={isSignUpLoading}
+                onClick={handleSignUpClick}
+              >
                 Cadastrar
               </Button>
             </ButtonGroup>
