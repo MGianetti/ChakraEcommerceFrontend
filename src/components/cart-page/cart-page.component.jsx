9262a19c1936fcc0881b8548517e9ec15d215976
Mateus Jesus <mateus_jesus@apple.com>
2021-12-14 23:12:36 -0300

    Conected CartPage to store

--- src/components/cart-page/cart-page.component.jsx
+++ src/components/cart-page/cart-page.component.jsx
@@ -5,10 +5,25 @@ import {
   ButtonGroup,
   Heading,
 } from '@chakra-ui/react';
+import { useEffect, useState } from 'react';
 
-import ChartIcon from './cart-item/cart-item.component';
+import { useSelector } from 'react-redux';
+
+import ChartItem from './cart-item/cart-item.component';
 
 export default function CartPage(props) {
+  const cartItems = useSelector(state => state.cart.items);
+  const [totalValue, setTotalValue] = useState(0);
+
+  useEffect(() => {
+    setTotalValue(
+      cartItems.reduce(
+        (accValue, currentItem) => accValue + currentItem.price,
+        0
+      )
+    );
+  }, [cartItems]);
+
   return (
     <>
       <Container maxWidth="1080px">
@@ -16,22 +31,12 @@ export default function CartPage(props) {
           <Heading>Meu carrinho</Heading>
         </Flex>
         <Flex w="100%" maxHeight="300px" direction="column" overflowY="scroll">
-          <ChartIcon />
-          <ChartIcon />
-          <ChartIcon />
-          <ChartIcon />
-          <ChartIcon />
-          <ChartIcon />
-          <ChartIcon />
-          <ChartIcon />
-          <ChartIcon />
-          <ChartIcon />
-          <ChartIcon />
-          <ChartIcon />
-          <ChartIcon />
+          {cartItems.map(cartItem => (
+            <ChartItem cartItem={cartItem} />
+          ))}
         </Flex>
         <Heading w="100%" textAlign="right" marginTop="25px">
-          Total: ${200}
+          Total: ${totalValue}
         </Heading>
         <Flex justify="center" alignItems="center" height="200px">
           <ButtonGroup variant="outline" spacing="6">
