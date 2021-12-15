ff40908509d0d2970a7493b34767e83f220a4fe4
Mateus Jesus <mateus_jesus@apple.com>
2021-12-14 23:13:48 -0300

    Implemented cart item dinamicity

--- src/components/cart-page/cart-item/cart-item.component.jsx
+++ src/components/cart-page/cart-item/cart-item.component.jsx
@@ -3,14 +3,16 @@ import { Flex, Heading, Box, Badge } from '@chakra-ui/react';
 import { FaTrashAlt } from 'react-icons/fa';
 
 export default function CartIcon(props) {
+  const { cartItem } = props;
+  const { productName, price } = cartItem;
   return (
     <Box w="100%" h="100" borderWidth="1px" borderRadius="lg" boxShadow="md">
       <Flex alignItems="center" margin="30px" direction="column">
         <Flex alignItems="center" justify="space-between" w="100%">
-          <Heading size="lg">Tênis</Heading>
+          <Heading size="lg">{productName}</Heading>
           <Flex alignItems="center" justify="space-evenly" w="20%">
             <Badge fontSize="1.5rem" colorScheme="green" height="max-content">
-              $ 200
+              $ {price}
             </Badge>
             <FaTrashAlt />
           </Flex>
