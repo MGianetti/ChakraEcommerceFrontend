bfb7c8a33cb50226dfe7a7ebac369a10fb1ac532
Mateus Jesus <mateus_jesus@apple.com>
2021-12-14 23:14:02 -0300

    Conected OrdersPage to store

--- src/components/orders-list-page/orders-list-page.component.jsx
+++ src/components/orders-list-page/orders-list-page.component.jsx
@@ -1,8 +1,20 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+import { useEffect } from 'react';
 import { Container, Flex, Heading } from '@chakra-ui/react';
+import { useSelector, useDispatch } from 'react-redux';
 
+import { getAllOrders } from '../../services/orders-service/orders-service.service';
+import { fetchAllOrders } from '../../store/orders/ordersSlice';
 import OrderCard from './order-card/order-card.component';
 
 export default function OrdersListPage(props) {
+  const dispatch = useDispatch();
+
+  useEffect(async () => {
+    const allItems = await getAllOrders();
+    dispatch(fetchAllOrders(allItems));
+  }, []);
+
   return (
     <>
       <Container maxWidth="1080px">
