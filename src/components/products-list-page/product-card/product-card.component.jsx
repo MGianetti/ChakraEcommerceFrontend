07e42cf5d70745f0171d2d358323ccea3a7fb9fe
Mateus Jesus <mateus_jesus@apple.com>
2021-12-14 23:14:33 -0300

    Implemented product card dinamicity

--- src/components/products-list-page/product-card/product-card.component.jsx
+++ src/components/products-list-page/product-card/product-card.component.jsx
@@ -1,28 +1,45 @@
 import { Badge, Box, Flex, Heading, Text } from '@chakra-ui/react';
 import { FaCartPlus } from 'react-icons/fa';
+import { useDispatch } from 'react-redux';
+
+import { addItemToCart } from '../../../store/cart/cartSlice';
 
 export default function ProductCard(props) {
+  const { itemForSale } = props;
+  const { id, productName, price, description } = itemForSale;
+  const itemKey = `product-key-${id}`;
+
+  const dispatch = useDispatch();
+
+  const handleAddToCartClick = item => {
+    dispatch(addItemToCart(item));
+  };
+
   return (
-    <Box w="100%" h="250" borderWidth="1px" borderRadius="lg" boxShadow="md">
+    <Box
+      key={itemKey}
+      w="100%"
+      h="250"
+      borderWidth="1px"
+      borderRadius="lg"
+      boxShadow="md"
+    >
       <Flex alignItems="center" margin="30px" direction="column">
         <Flex alignItems="center" justify="space-between" w="100%">
-          <Heading size="lg">Tênis</Heading>
+          <Heading size="lg">{productName}</Heading>
           <Badge size="xs" colorScheme="green" height="max-content">
-            $ 200
+            $ {price}
           </Badge>
         </Flex>
         <Heading size="xs">Descrição</Heading>
         <Text margin="10px" maxHeight="90px" overflowY="scroll" border="1px">
-          Aenean non lorem nisl. Vivamus vel justo malesuada, egestas nisl vel,
-          molestie leo. Aenean non lorem nisl. Vivamus vel justo malesuada,
-          egestas nisl vel, molestie leo. Aenean non lorem nisl. Vivamus vel
-          justo malesuada, egestas nisl vel, molestie leo. Aenean non lorem
-          nisl. Vivamus vel justo malesuada, egestas nisl vel, molestie leo.
-          Aenean non lorem nisl. Vivamus vel justo malesuada, egestas nisl vel,
-          molestie leo.
+          {description}
         </Text>
         <Flex w="100%" justifyContent="end" marginTop="15px">
-          <FaCartPlus width="100%" />
+          <FaCartPlus
+            width="100%"
+            onClick={() => handleAddToCartClick(itemForSale)}
+          />
         </Flex>
       </Flex>
     </Box>
